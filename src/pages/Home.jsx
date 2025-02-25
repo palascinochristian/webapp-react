2668bd22e8cf507783e0ba8b2b3bc6f0d74424ea
palascinochristian <palascinochristian@gmail.com>
2025-02-25 17:34:23 +0100

    feat: add db connection to movies on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import axios from "../api/axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <h1>Home Page Movies</h1>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = () => {
+    axios.get("/movies").then((res) => {
+      setMovies(res.data);
+    });
+  };
+
+  useEffect(fetchMovies, []);
+
+  return (
+    <>
+      <h1> Movies List </h1>
+      <hr></hr>
+      {movies.map((movie) => (
+        <div key={movie.id}>{movie.title}</div>
+      ))}
+    </>
+  );
 }
