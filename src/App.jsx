210b061c5d1d5f98656d8bf9262f79f3690e5ee2
palascinochristian <palascinochristian@gmail.com>
2025-02-25 17:09:49 +0100

    feat: added main components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
-export default function App() {}
+import { BrowserRouter, Routes, Route } from "react-router";
+//Layout
+import MainLayout from "./layouts/MainLayout";
+//Pages
+import Home from "./pages/Home";
+import MoviePage from "./pages/MoviePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
