e31235d9fecd210fa1d5c2588ca15000d4c634d5
palascinochristian <palascinochristian@gmail.com>
2025-02-25 18:29:30 +0100

    feat: added Links

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router";
+
 export default function Logo() {
-  return <img src="../logo.png" className="h-50" alt="Movies 138 Logo" />;
+  return (
+    <Link to="/" className="h-50">
+      <img src="../logo.png" className="h-50" alt="Movies 138 Logo" />
+    </Link>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "../api/axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -18,9 +19,13 @@ export default function Home() {
       <hr className="border-gray-600 mb-4" />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {movies.map((movie) => (
-          <div key={movie.id} className="bg-gray-800 p-4 rounded-lg shadow-lg">
+          <Link
+            key={movie.id}
+            to={`/movies/${movie.id}`}
+            className="bg-gray-800 p-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+          >
             <h2 className="text-lg font-semibold">{movie.title}</h2>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
