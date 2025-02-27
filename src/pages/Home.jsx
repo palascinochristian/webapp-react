9d7ebe633de7b417cd0e2788617ada6d607bedb9
palascinochristian <palascinochristian@gmail.com>
2025-02-27 20:50:05 +0100

    feat: added movies cover in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,9 +22,18 @@ export default function Home() {
           <Link
             key={movie.id}
             to={`/movies/${movie.id}`}
-            className="bg-gray-800 p-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105"
+            className="bg-gray-800 p-4 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 flex flex-col"
           >
-            <h2 className="text-lg font-semibold">{movie.title}</h2>
+            <div className="h-130">
+              <img
+                className="w-full h-full object-cover rounded-lg"
+                src={`http://localhost:3001/movies/${movie.image}`}
+                alt={movie.title}
+              />
+            </div>
+            <h2 className="text-lg font-semibold mt-3 text-center">
+              {movie.title}
+            </h2>
           </Link>
         ))}
       </div>
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div className="container mx-auto p-6 text-white">
-      <h1 className="text-4xl font-bold text-red-500">
+      <h1 className="text-4xl font-bold text-blue-900">
         La pagina che stai cercando non esiste
       </h1>
     </div>
